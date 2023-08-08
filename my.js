b3dea2f65c05c4165f58033be35baa0230e78386
motogoa <107267164+motogoa@users.noreply.github.com>
2023-08-08 14:27:27 +0400

    Update my.js

--- my.js
+++ my.js
@@ -1,10 +1,31 @@
 function testJSON(){
-const url ="https://script.google.com/macros/s/AKfycbzuEz7EPa84lNG16wJaqh20GIvfXOPQlQWe9TF8GgbZ93EXLOE9bWLWwqImQRbGolo-/exec"
-fetch (url)
+    // this is the url of the "try if JSON works" appscript attached to the "JSON API, Wep App" googlesheet
+    const url ="https://script.google.com/macros/s/AKfycbx-hBT3WzH0q67H0NmW0ZPm8fn_860CLjYStEwQghR6Dimtl4LJOhhE1k0kX5-IdEWa/exec"
+    
+
+    // this gets data from a appscript
+    fetch (url)
     .then(d => d.json ())
     .then(d => {
         document.getElementById("app").textContent = d[0].status;
     })
 }
 
+function addRow(){
+    // this is the url of the "try if JSON works" appscript attached to the "JSON API, Wep App" googlesheet
+    const url ="https://script.google.com/macros/s/AKfycbx-hBT3WzH0q67H0NmW0ZPm8fn_860CLjYStEwQghR6Dimtl4LJOhhE1k0kX5-IdEWa/exec"
+    
+    
+    // this gets data from a appscript
+    fetch (url,{
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        mode: "no-cors", // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        redirect: "follow", // manual, *follow, error
+        body: JSON.stringify({name: "Olive Oyle"}), // body data type must match "Content-Type" header
+      })
+        
+}
+
 document.getElementById("btn").addEventListener("click", testJSON)
+document.getElementById("btn2").addEventListener("click", addRow)
